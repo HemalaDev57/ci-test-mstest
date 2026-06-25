868deda34d8c1cca1a9e0c6e289a36ffd3541adb
HemalaDev57 <hdevanan@cloudbees.com>
2026-06-25 12:19:33 +0530

    Initial: ci-test-mstest test suite with GitHub Actions workflow

---
+++ MyProject.Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyProject.Tests;
+
+[TestClass]
+public class CalculatorTests
+{
+    [TestMethod]
+    public void TestAddNumbers() { Assert.AreEqual(5, 2 + 3); }
+
+    [TestMethod]
+    public void TestSubtractNumbers() { Assert.AreEqual(1, 3 - 2); }
+
+    [TestMethod]
+    public void TestSubtractNumbers_FailsWhenNegative() { Assert.AreEqual(0, 5 - 9); } // deliberate fail
+
+    [TestMethod]
+    [Ignore("Not implemented yet")]
+    public void TestMultiplyByZero() { Assert.AreEqual(0, 5 * 0); }
+
+    [TestMethod]
+    [ExpectedException(typeof(DivideByZeroException))]
+    public void TestDivideByZero() { int x = 1 / 0; }
+}
